6e9e7b88701b9a4f204cbfe399eea97c8384f9f4
Martin Sonda <martindanielsonda@gmail.com>
2026-07-09 18:08:30 -0300

    feat: configuración del servidor Express

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 import productsRouter from "./src/routes/products.router.js";
+import authRouter from "./src/routes/auth.router.js";
 
 const app = express();
-
-app.use("/api/products", productsRouter);
-
 const PORT = process.env.PORT || 3000;
 
+// Middlewares globales
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rutas
+app.use("/api/products", productsRouter);
+app.use("/auth", authRouter);
+
+// Middleware para rutas no definidas
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Ruta no encontrada",
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
